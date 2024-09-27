985f5e5a9a01a7f852490f8234d5a313badf7fa5
Katerina Terwelp <kterwelp@xsede.org@c3cpu-c15-u32-4.rc.int.colorado.edu>
2024-09-27 09:46:43 -0600

    Organize commands in script folder and create batch job version

---
+++ scripts/bv_brc.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+
+# set up user defined variables 
+taxonIDs=(287 1280)
+
+taxonNames=("Staphylococcus aureus" "Pseudomonas aeruginosa")
+
+# set up automatic variables
+drugFields="$(p3-get-genome-drugs --fields | tr '\n' ',')"
+dataFields="$(p3-get-genome-data --fields | sed 's/([^)]*)//g' | tr '\n' ',' | sed 's/ ,/,/g')"
+
+# acquire list of genomes using both methods
+
+cd "$projectDir" 
+
+mkdir -p "genomes" "metadata" 
+
+for ID in "${taxonIDs[@]}"
+do 
+echo "Generating genomes for taxonID: $ID" 
+
+p3-all-genomes \
+--in taxon_id,"$ID" \
+--eq genome_quality,Good \
+--in genome_status,WGS,Complete > "genomes/${ID}.genomes.txt" 
+
+done
+
+for name in "${taxonNames[@]}"
+do
+echo "Generating genomes for taxonName: $name" 
+nameFixed="${name// /-}"
+echo "${nameFixed}.genomes.txt"
+
+p3-all-genomes \
+--eq "genome_name,${name}" \
+--eq genome_quality,Good \
+--in genome_status,WGS,Complete > "genomes/${nameFixed}.genomes.txt" 
+
+done
+
+# download metadata for drugs and genomes
+
+for file in genomes/*
+do 
+echo "Downloading metadata for genomes in $file" 
+name=$(basename "$file" ".genomes.txt")
+echo "creating ${name}_data.tsv"
+
+cat "$file" | \
+p3-get-genome-data \
+--attr $dataFields > "metadata/${name}_data.tsv"
+
+echo "finished downloading ${name}_data.tsv" 
+
+echo "creating ${name}_drug.tsv" 
+
+cat "$file" | \
+p3-get-genome-drugs \
+--required resistant_phenotype \
+--required antibiotic \
+--attr $drugFields > "metadata/${name}_drug.tsv"
+
+echo "finished downloading ${name}_drug.tsv" 
+
+done
